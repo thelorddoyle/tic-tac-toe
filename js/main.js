c00006afe76d63608196438279c7eb467fa95faa
Your Name <daniel@Daniels-MBP-2.modem>
2022-01-10 22:13:55 +1100

    Lots of small improvements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // variables
-
 turnTracker = 1;
 let squaresClicked = [];
 let squaresClickedByPlayer1 = [];
@@ -30,8 +29,51 @@ $( document ).ready(function() {
             squaresClickedByPlayer2.push(squareNumber)
             console.log('clicked by player 2: ', squaresClickedByPlayer2)
         }
-
     } // end of isClicked
+
+    const disableBoardClicks = function () {
+        $('.boardSquare').css({'pointer-events': 'none'})
+    } // end of disableBoardClicks()
+
+    const enableBoardClicks = function () {
+        $('.boardSquare').css({'pointer-events': 'all'})
+    } // end of disableBoardClicks()
+
+    const resetBoard = function () {
+        squaresClicked = [];
+        squaresClickedByPlayer1 = [];
+        squaresClickedByPlayer2 = [];
+        currentPlayer = 'Player1';
+        turnTracker = 1;
+        $('.boardSquare').css({ 'backgroundImage': 'none' })
+    } // end of resetBoard()
+
+    const startOver = function () {
+        window. location. reload()
+    } // end of startOver()
+
+    const showPlayAgainButton = function () {
+        $('input#nextGame').fadeIn()
+    }
+
+    const hidePlayAgainButton = function () {
+        $('input#nextGame').fadeOut()
+    }
+
+    const announceWinner = function (h3node, winner, wins) {
+
+        const showWinner = function (h3node, winner, wins) {
+
+            $(`h3#${h3node}`).html(`Score: ${wins}`)
+
+            let $winnerMessage = $('<h3 id="winner-message">')
+            $winnerMessage.html(`${winner} wins this game!`)
+            $('div#gameStarter').prepend($winnerMessage)
+        }
+
+        showWinner(h3node, winner, wins)
+        showPlayAgainButton()
+    }
 
     const checkIfWon = function (player) {
 
@@ -47,7 +89,16 @@ $( document ).ready(function() {
             || (squaresClickedByPlayer1.includes('square4') && squaresClickedByPlayer1.includes('square5') && squaresClickedByPlayer1.includes('square6'))
             || (squaresClickedByPlayer1.includes('square7') && squaresClickedByPlayer1.includes('square8') && squaresClickedByPlayer1.includes('square9'))
             ) {
-                console.log('You win')
+
+                player1wins++
+                disableBoardClicks()
+                announceWinner('player1score', 'Player 1', player1wins)
+
+            } 
+            
+            // TODO: Do cat animation and call it a cats game
+            else if (squaresClicked.length === 9) {
+                console.log('DRAW')
             }
 
         } else {
@@ -62,11 +113,18 @@ $( document ).ready(function() {
             || (squaresClickedByPlayer2.includes('square7') && squaresClickedByPlayer2.includes('square8') && squaresClickedByPlayer2.includes('square9'))
             ) {
 
-                console.log('You win')
+                player2wins++
+                disableBoardClicks()
+                announceWinner('player2score', 'Player 2', player2wins)
 
+            } 
+            
+            // TODO: Do cat animation and call it a cats game
+            else if (squaresClicked.length === 9) {
+                console.log('DRAW')
             }
         }
-    }; // checkIfWon()
+    }; // end of checkIfWon()
 
     $('.boardSquare').on('click', function () {
 
@@ -94,25 +152,15 @@ $( document ).ready(function() {
         }
     });
 
-    const resetBoard = function () {
-        squaresClicked = [];
-        squaresClickedByPlayer1 = [];
-        squaresClickedByPlayer2 = [];
-        currentPlayer = 'Player1';
-        turnTracker = 1;
-        $('.boardSquare').css({ 'backgroundImage': 'none' })
-    } // resetBoard
-
-    const startOver = function () {
-        window. location. reload()
-    } // resetBoard
-
-    $('input#reset').on('click', function () {
-        resetBoard()
-    })
-
     $('input#startOver').on('click', function () {
         startOver()
+    })
+
+    $('input#nextGame').on('click', function () {
+        resetBoard()
+        hidePlayAgainButton()
+        enableBoardClicks()
+        $('h3#winner-message').remove()
     })
         
 }); // document.ready()
