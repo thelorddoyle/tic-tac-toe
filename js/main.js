c4dfada48472e52de0a3f12db010a968e73ecf4d
Your Name <daniel@Daniels-MBP-2.modem>
2022-01-10 20:53:33 +1100

    done a bunch of graphical work and UI / nice to have stuff

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+// variables
+
 turnTracker = 1;
-let squaresClicked = []
-let squaresClickedByPlayer1 = []
-let squaresClickedByPlayer2 = []
+let squaresClicked = [];
+let squaresClickedByPlayer1 = [];
+let squaresClickedByPlayer2 = [];
+let player1wins = 0;
+let player2wins = 0;
 let currentPlayer = 'Player1'
 
 $( document ).ready(function() {
     console.log( "document ready!" );
 
-    // also inside Dom Function. simply finds out whose turn it is.
     const findOutPlayer = function () {
         if (turnTracker % 2 !== 0) {
             currentPlayer = 'Player1'
@@ -18,7 +21,6 @@ $( document ).ready(function() {
         }
     } // end of findOutPlayer
 
-    // this function is inside the dom ready. it is simply taking the square id and passing it in to an overall list of squares 'owned' by the player
     const isClicked = function(squareNumber) {
         squaresClicked.push(squareNumber);
         if (currentPlayer === 'Player1') {
@@ -31,12 +33,48 @@ $( document ).ready(function() {
 
     } // end of isClicked
 
+    const checkIfWon = function (player) {
+
+        if (player === 'Player1') {
+
+            if 
+            ((squaresClickedByPlayer1.includes('square1') && squaresClickedByPlayer1.includes('square2') && squaresClickedByPlayer1.includes('square3')) 
+            || (squaresClickedByPlayer1.includes('square1') && squaresClickedByPlayer1.includes('square4') && squaresClickedByPlayer1.includes('square7'))
+            || (squaresClickedByPlayer1.includes('square1') && squaresClickedByPlayer1.includes('square5') && squaresClickedByPlayer1.includes('square9'))
+            || (squaresClickedByPlayer1.includes('square2') && squaresClickedByPlayer1.includes('square5') && squaresClickedByPlayer1.includes('square8'))
+            || (squaresClickedByPlayer1.includes('square3') && squaresClickedByPlayer1.includes('square5') && squaresClickedByPlayer1.includes('square7'))
+            || (squaresClickedByPlayer1.includes('square3') && squaresClickedByPlayer1.includes('square6') && squaresClickedByPlayer1.includes('square9'))
+            || (squaresClickedByPlayer1.includes('square4') && squaresClickedByPlayer1.includes('square5') && squaresClickedByPlayer1.includes('square6'))
+            || (squaresClickedByPlayer1.includes('square7') && squaresClickedByPlayer1.includes('square8') && squaresClickedByPlayer1.includes('square9'))
+            ) {
+                console.log('You win')
+            }
+
+        } else {
+
+            if ((squaresClickedByPlayer2.includes('square1') && squaresClickedByPlayer2.includes('square2') && squaresClickedByPlayer2.includes('square3')) 
+            || (squaresClickedByPlayer2.includes('square1') && squaresClickedByPlayer2.includes('square4') && squaresClickedByPlayer2.includes('square7'))
+            || (squaresClickedByPlayer2.includes('square1') && squaresClickedByPlayer2.includes('square5') && squaresClickedByPlayer2.includes('square9'))
+            || (squaresClickedByPlayer2.includes('square2') && squaresClickedByPlayer2.includes('square5') && squaresClickedByPlayer2.includes('square8'))
+            || (squaresClickedByPlayer2.includes('square3') && squaresClickedByPlayer2.includes('square5') && squaresClickedByPlayer2.includes('square7'))
+            || (squaresClickedByPlayer2.includes('square3') && squaresClickedByPlayer2.includes('square6') && squaresClickedByPlayer2.includes('square9'))
+            || (squaresClickedByPlayer2.includes('square4') && squaresClickedByPlayer2.includes('square5') && squaresClickedByPlayer2.includes('square6'))
+            || (squaresClickedByPlayer2.includes('square7') && squaresClickedByPlayer2.includes('square8') && squaresClickedByPlayer2.includes('square9'))
+            ) {
+
+                console.log('You win')
+
+            }
+        }
+    }; // checkIfWon()
+
     $('.boardSquare').on('click', function () {
 
         $square = $(this)
         idOfSquare = $(this).attr('id');
 
-        const putInSymbol = function (squareToChange) {
+
+        const putInSymbol = function () {
             if (currentPlayer === 'Player1') {
                 $square.css({'backgroundImage': 'url(images/x.png)'})
             }
@@ -46,33 +84,35 @@ $( document ).ready(function() {
         }
 
         if ((squaresClicked.includes(idOfSquare) === false)) {
+            findOutPlayer()
             turnTracker++
             putInSymbol(idOfSquare);
             isClicked(idOfSquare)
-            checkIfWon(findOutPlayer())
+            checkIfWon(currentPlayer)
         } else {
             console.log('Already clicked this square!')
         }
     });
 
-    const checkIfWon = function (player) {
-        if ($('#square1').data('clickedby') === player && $('#square2').data('clickedby') === player && $('#square3').data('clickedby') === player) {  
-            console.log('You win')
-        } else if ($('#square1').data('clickedby') === player && $('#square4').data('clickedby') === player && $('#square7').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square1').data('clickedby') === player && $('#square5').data('clickedby') === player && $('#square9').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square2').data('clickedby') === player && $('#square5').data('clickedby') === player && $('#square8').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square3').data('clickedby') === player && $('#square5').data('clickedby') === player && $('#square7').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square3').data('clickedby') === player && $('#square6').data('clickedby') === player && $('#square9').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square4').data('clickedby') === player && $('#square5').data('clickedby') === player && $('#square6').data('clickedby') === player) {
-            console.log('You win')
-        } else if ($('#square7').data('clickedby') === player && $('#square8').data('clickedby') === player && $('#square9').data('clickedby') === player) {
-            console.log('You win')
-        }
-    }
+    const resetBoard = function () {
+        squaresClicked = [];
+        squaresClickedByPlayer1 = [];
+        squaresClickedByPlayer2 = [];
+        currentPlayer = 'Player1';
+        turnTracker = 1;
+        $('.boardSquare').css({ 'backgroundImage': 'none' })
+    } // resetBoard
 
-});
+    const startOver = function () {
+        window. location. reload()
+    } // resetBoard
+
+    $('input#reset').on('click', function () {
+        resetBoard()
+    })
+
+    $('input#startOver').on('click', function () {
+        startOver()
+    })
+        
+}); // document.ready()
